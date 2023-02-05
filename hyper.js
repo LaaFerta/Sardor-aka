96394f3eb05ac6528be8867af833738bff9cdd05
IC Deis <Rennarion@gmail.com>
2023-02-05 17:39:43 +0500

    tags edited

--- hyper.js
+++ hyper.js
@@ -3,13 +3,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const severless = require("serverless-http")
 const mongoose = require('mongoose')
-const { MONGO_URL } = require('./config/keys')
 const routeAuth = require('./routes//routeAuth')
 const routeGoods = require('./routes/routeGoods')
 const routeDebt = require('./routes/routeDebt')
 const routeCategory = require('./routes/routeCategory')
 const { auth } = require('./middleware/authentication')
+const { MONGO_URL } = require('./config/keys')
+
 
 const PORT = process.env.PORT || 1000
 const HOST = process.env.HOST
@@ -19,7 +21,7 @@ mongoose.connect(MONGO_URL, () => console.log('Connected to MongoDB'))
 
 app.use(express.json({ limit: '25mb' }))
 app.use(express.urlencoded({ extended: true, limit: '25mb' }))
-app.use(cors({origin: '*', credentials: true}))
+app.use(cors({ origin: '*', credentials: true }))
 
 app.use('/auth', routeAuth)
 app.use('/goods', auth, routeGoods)
@@ -29,3 +31,7 @@ app.use('/category', auth, routeCategory)
 app.get('/', (req, res) => {
    res.status(200).send("Assalamu 'Alaykum")
 })
+
+module.exports.controller = async (context, req) => {
+   context.res = await handler(context, req);
+}
--- models/Goods.js
+++ models/Goods.js
@@ -6,7 +6,7 @@ const Joi = require('joi')
 const GoodsSchema = new Schema({
    name: { type: String, required: true },
    price: { type: Number, required: true },
-   tag: [{type: String, required: true}],
+   tag: {type: String, required: true},
    imgUrl: { type: String },
    updatedAt: {type: Date, required: true}
 })
@@ -17,7 +17,7 @@ function validateGoods(goods) {
    const validGoods = Joi.object({
       name: Joi.string().min(2).max(70).required(),
       price: Joi.number().min(500).max(9999999).required(),
-      tag: Joi.array().items(Joi.string().min(3).max(50)),
+      tag: Joi.string().min(3).max(50),
       imgUrl: Joi.string(),
       updatedAt: Joi.date().required()
    })
